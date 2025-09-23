eb7ee1172f8596db04f1ab13a082beb0f405262e
maheshghatole <50540683+maheshghatole@users.noreply.github.com>
2025-09-23 16:44:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Burger menu toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
